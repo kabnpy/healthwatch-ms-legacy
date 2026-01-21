e53c9833f4500181e5d9cce0da298956e84b0a83
kabnpy <kabnpy@gmail.com>
2026-01-22 00:32:30 +0300

    feat(backend): automate mock data seeding in local environment

--- backend/app/initial_data.py
+++ backend/app/initial_data.py
@@ -2,7 +2,9 @@ import logging
 
 from sqlmodel import Session
 
+from app.core.config import settings
 from app.core.db import engine, init_db
+from app.seed_mock_data import create_mock_data
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -11,6 +13,10 @@ logger = logging.getLogger(__name__)
 def init() -> None:
     with Session(engine) as session:
         init_db(session)
+    
+    if settings.ENVIRONMENT == "local":
+        logger.info("Local environment detected, seeding mock data...")
+        create_mock_data()
 
 
 def main() -> None:
